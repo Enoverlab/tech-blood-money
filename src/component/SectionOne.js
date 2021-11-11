905f108efb3d826c793d61da6119004bda6f7576
EmmanuelOluwafemi <seek9ja@gmail.com>
2021-11-11 09:01:14 +0100

    added update

--- src/component/SectionOne.js
+++ src/component/SectionOne.js
@@ -1,16 +1,10 @@
-import {useEffect} from 'react'
 import logo from "../assets/logo.svg"
 import shape from '../assets/backgroundshape.png'
 import background from '../assets/rec.png'
 import book from '../assets/book1.png'
 import styled, {keyframes} from 'styled-components'
-import Aos from 'aos';
-import "aos/dist/aos.css"
 
 const SectionOne = () => {
-    useEffect(()=>{
-        Aos.init({duration : 2000})
-    },[])
     return (
         <Styledone>
             <img src={logo} alt="" className='logo' />
